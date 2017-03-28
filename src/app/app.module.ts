522798050da955ca9c328712946f2651060b93ac
Pragadeeswar <pviswanathan@nationalsystems.com>
2017-03-27 22:32:30 -0500

    added material design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {MaterialModule} from '@angular/material';
+import 'hammerjs';
+
+
 
 import { AppComponent } from './app.component';
 
@@ -10,6 +14,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
+    MaterialModule.forRoot(),
     BrowserModule,
     FormsModule,
     HttpModule
